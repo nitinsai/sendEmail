789e42bb3851f1e309e94c6674f50adb1cd84bd9
Narayanan <nbhoosan@cisco.com>
2015-06-01 19:11:07 +0530

    Second Commit

--- app.js
+++ app.js
@@ -6,6 +6,21 @@
 var express = require('express');
 var app = module.exports = express();
 var  path = require('path');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+app.use(allowCrossDomain)
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.cookieParser());
